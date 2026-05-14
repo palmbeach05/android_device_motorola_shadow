Quarx2k/android_device_moto_jordan-common

Remove bootmenu, use new safestrap

BoardConfig.mk
@@ -91,16 +91,6 @@ TARGET_USE_OMAP_COMPAT  := true
 BUILD_WITH_TI_AUDIO := 1
 BUILD_PV_VIDEO_ENCODERS := 1
 
-# Bootmenu
-BOARD_USES_BOOTMENU := true
-BOARD_WITH_CPCAP    := true
-BOARD_MMC_DEVICE    := /dev/block/mmcblk1
-BOARD_SDCARD_DEVICE_PRIMARY   := /dev/block/mmcblk0p1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk0
-BOARD_SDEXT_DEVICE  := /dev/block/mmcblk0p2
-BOARD_SYSTEM_DEVICE := /dev/block/mmcblk1p21
-BOARD_DATA_DEVICE   := /dev/block/mmcblk1p25
-
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_FSTAB := device/moto/jordan-common/recovery.fstab
@@ -123,7 +113,7 @@ TW_NO_BATT_PERCENT := false
 TW_NO_REBOOT_RECOVERY := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_ALWAYS_RMRF := true
-TARGET_RECOVERY_PRE_COMMAND := "/system/bootmenu/script/reboot_command.sh"
+TARGET_RECOVERY_PRE_COMMAND := "/system/bootstrap/script/reboot_command.sh"
 TARGET_RECOVERY_PRE_COMMAND_CLEAR_REASON := true
 BOARD_UMS_LUNFILE := /sys/class/android_usb/f_mass_storage/lun/file
 TW_NO_SCREEN_BLANK := true
@@ -205,7 +195,7 @@ hboot:
 	mkdir -p $(PRODUCT_OUT)/system/bootmenu/2nd-boot   
 	echo "$(BOARD_KERNEL_CMDLINE)" > $(PRODUCT_OUT)/system/bootmenu/2nd-boot/cmdline  
 	make -C  $(ANDROID_BUILD_TOP)/device/moto/jordan-common/hboot ARCH=arm CROSS_COMPILE="arm-eabi-"
-	mv $(ANDROID_BUILD_TOP)/device/moto/jordan-common/hboot/hboot.bin $(PRODUCT_OUT)/system/bootmenu/2nd-boot/
+	mv $(ANDROID_BUILD_TOP)/device/moto/jordan-common/bootstrap/hboot/hboot.bin $(PRODUCT_OUT)/system/bootstrap/2nd-boot/
 	make clean -C $(ANDROID_BUILD_TOP)/device/moto/jordan-common/hboot
 
 # If kernel sources are present in repo, here is the location

bootstrap/bootstrap.mk
@@ -1,0 +1,47 @@
+# Required tools and blobs for bootstrap
+bm_device = device/moto/jordan-common
+include $(all-subdir-makefiles)
+
+# 2nd-boot profiles
+PRODUCT_COPY_FILES += \
+	${bm_device}/profiles/2nd-boot/hbootmod.ko:system/bootstrap/2nd-boot/hbootmod.ko \
+	${bm_device}/profiles/2nd-boot/hboot.cfg:system/bootstrap/2nd-boot/hboot.cfg \
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+	${bm_device}/profiles/ramdisk/ueventd.mapphone_umts.rc:root/ueventd.mapphone_umts.rc \
+	${bm_device}/profiles/ramdisk/init.usb.rc:root/init.usb.rc \
+	${bm_device}/profiles/ramdisk/init.mapphone_umts.rc:root/init.mapphone_umts.rc \
+	${bm_device}/profiles/ramdisk/fstab.mapphone_umts:root/fstab.mapphone_umts \
+
+# scripts
+PRODUCT_COPY_FILES += \
+	${bm_device}/bootstrap/script/2nd-boot.sh:system/bootstrap/script/2nd-boot.sh \
+	${bm_device}/bootstrap/script/reboot_command.sh:system/bootstrap/script/reboot_command.sh \
+	${bm_device}/bootstrap/script/recovery.sh:system/bootstrap/script/recovery.sh \
+	${bm_device}/bootstrap/script/pdsbackup.sh:system/bootstrap/script/pdsbackup.sh \
+
+# prebuilt modules for stock kernel
+PRODUCT_COPY_FILES += \
+	${bm_device}/bootstrap/modules/jbd2.ko:system/bootstrap/modules/jbd2.ko \
+	${bm_device}/bootstrap/modules/ext4.ko:system/bootstrap/modules/ext4.ko \
+
+# images
+PRODUCT_COPY_FILES += \
+	${bm_device}/bootstrap/images/background-def.png:system/bootstrap/images/background-def.png \
+	${bm_device}/bootstrap/images/background-blank.png:system/bootstrap/images/background-blank.png \
+
+# prebuilt binaries
+PRODUCT_COPY_FILES += \
+	${bm_device}/bootstrap/binary/logwrapper:system/bin/logwrapper \
+	${bm_device}/bootstrap/binary/logwrapper.bin:system/bin/logwrapper.bin \
+	${bm_device}/profiles/2nd-boot/hboot_recovery.cfg:system/bootstrap/2nd-boot/hboot_recovery.cfg \
+	${bm_device}/bootstrap/binary/hbootuser:system/bootstrap/2nd-boot/hbootuser \
+	${bm_device}/bootstrap/binary/safestrapmenu:system/bootstrap/2nd-boot/safestrapmenu \
+	${bm_device}/bootstrap/binary/busybox:system/bootstrap/binary/busybox \
+	$(bm_device)/twrp.fstab:recovery/root/etc/twrp.fstab \
+	$(OUT)/ramdisk-recovery.img:system/bootstrap/2nd-boot/ramdisk-recovery \
+	${bm_device}/profiles/2nd-boot/zImage-recovery:system/bootstrap/2nd-boot/zImage-recovery \
+	$(OUT)/ramdisk.img:system/bootstrap/2nd-boot/ramdisk \
+	$(OUT)/kernel:system/bootstrap/2nd-boot/zImage
+

device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)	
 # Blobs and bootmenu stuff
 $(call inherit-product, device/moto/jordan-common/jordan-blobs.mk)
-$(call inherit-product, device/moto/jordan-common/bootmenu/bootmenu.mk)
+$(call inherit-product, device/moto/jordan-common/bootstrap/bootstrap.mk)
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
@@ -97,7 +97,7 @@ PRODUCT_PACKAGES += \
 	libaudioutils audio.a2dp.default  \
 	libaudiohw_legacy \
 
-#	 OMX stuff
+# OMX stuff
 PRODUCT_PACKAGES += dspexec libbridge libLCML libOMX_Core libstagefrighthw
 PRODUCT_PACKAGES += libOMX.TI.AAC.encode libOMX.TI.AAC.decode libOMX.TI.AMR.decode libOMX.TI.AMR.encode
 PRODUCT_PACKAGES += libOMX.TI.WBAMR.encode libOMX.TI.MP3.decode libOMX.TI.WBAMR.decode
@@ -123,12 +123,7 @@ PRODUCT_PACKAGES += \
     hostapd.conf \
     wpa_supplicant.conf \
     regulatory.bin \
-    calibrator \
     ti_wfd_libs \
-
-PRODUCT_COPY_FILES += \
-    $(OUT)/ramdisk.img:system/bootmenu/2nd-boot/ramdisk \
-    $(OUT)/kernel:system/bootmenu/2nd-boot/zImage
 
 # Should be after the full_base include, which loads languages_full
 PRODUCT_LOCALES := en_US en_GB en_IN fr_FR it_IT de_DE es_ES hu_HU uk_UA zh_CN zh_TW ru_RU nl_NL se_SV cs_CZ pl_PL pt_BR da_DK ko_KR el_GR ro_RO iw_IL ar_EG

palmbeach05/android_device_motorola_shadow

BoardConfig.mk
@@ -91,16 +91,6 @@ TARGET_USE_OMAP_COMPAT  := true
 BUILD_WITH_TI_AUDIO := 1
 BUILD_PV_VIDEO_ENCODERS := 1
 
-# Bootmenu
-BOARD_USES_BOOTMENU := true
-BOARD_WITH_CPCAP    := true
-BOARD_MMC_DEVICE    := /dev/block/mmcblk1
-BOARD_SDCARD_DEVICE_PRIMARY   := /dev/block/mmcblk0p1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk0
-BOARD_SDEXT_DEVICE  := /dev/block/mmcblk0p2
-BOARD_SYSTEM_DEVICE := /dev/block/mmcblk1p21
-BOARD_DATA_DEVICE   := /dev/block/mmcblk1p24
-
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_FSTAB := device/moto/shadow-common/recovery.fstab
@@ -123,7 +113,7 @@ TW_NO_BATT_PERCENT := false
 TW_NO_REBOOT_RECOVERY := true
 TW_NO_REBOOT_BOOTLOADER := true
 TW_ALWAYS_RMRF := true
-TARGET_RECOVERY_PRE_COMMAND := "/system/bootmenu/script/reboot_command.sh"
+TARGET_RECOVERY_PRE_COMMAND := "/system/bootstrap/script/reboot_command.sh"
 TARGET_RECOVERY_PRE_COMMAND_CLEAR_REASON := true
 BOARD_UMS_LUNFILE := /sys/class/android_usb/f_mass_storage/lun/file
 TW_NO_SCREEN_BLANK := true
@@ -205,7 +195,7 @@ hboot:
 	mkdir -p $(PRODUCT_OUT)/system/bootmenu/2nd-boot   
 	echo "$(BOARD_KERNEL_CMDLINE)" > $(PRODUCT_OUT)/system/bootmenu/2nd-boot/cmdline  
 	make -C  $(ANDROID_BUILD_TOP)/device/moto/shadow-common/hboot ARCH=arm CROSS_COMPILE="arm-eabi-"
-	mv $(ANDROID_BUILD_TOP)/device/moto/shadow-common/hboot/hboot.bin $(PRODUCT_OUT)/system/bootmenu/2nd-boot/
+	mv $(ANDROID_BUILD_TOP)/device/moto/shadow-common/bootstrap/hboot/hboot.bin $(PRODUCT_OUT)/system/bootstrap/2nd-boot/
 	make clean -C $(ANDROID_BUILD_TOP)/device/moto/shadow-common/hboot
 
 # If kernel sources are present in repo, here is the location

bootstrap/bootstrap.mk
@@ -1,0 +1,47 @@
+# Required tools and blobs for bootstrap
+bm_device = device/moto/shadow-common
+include $(all-subdir-makefiles)
+
+# 2nd-boot profiles
+PRODUCT_COPY_FILES += \
+	${bm_device}/profiles/2nd-boot/hbootmod.ko:system/bootstrap/2nd-boot/hbootmod.ko \
+	${bm_device}/profiles/2nd-boot/hboot.cfg:system/bootstrap/2nd-boot/hboot.cfg \
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+	${bm_device}/profiles/ramdisk/ueventd.mapphone_cdma.rc:root/ueventd.mapphone_cdma.rc \
+	${bm_device}/profiles/ramdisk/init.usb.rc:root/init.usb.rc \
+	${bm_device}/profiles/ramdisk/init.mapphone_cdma.rc:root/init.mapphone_cdma.rc \
+	${bm_device}/profiles/ramdisk/fstab.mapphone_cdma:root/fstab.mapphone_cdma \
+
+# scripts
+PRODUCT_COPY_FILES += \
+	${bm_device}/bootstrap/script/2nd-boot.sh:system/bootstrap/script/2nd-boot.sh \
+	${bm_device}/bootstrap/script/reboot_command.sh:system/bootstrap/script/reboot_command.sh \
+	${bm_device}/bootstrap/script/recovery.sh:system/bootstrap/script/recovery.sh \
+	${bm_device}/bootstrap/script/pdsbackup.sh:system/bootstrap/script/pdsbackup.sh \
+
+# prebuilt modules for stock kernel
+PRODUCT_COPY_FILES += \
+	${bm_device}/bootstrap/modules/jbd2.ko:system/bootstrap/modules/jbd2.ko \
+	${bm_device}/bootstrap/modules/ext4.ko:system/bootstrap/modules/ext4.ko \
+
+# images
+PRODUCT_COPY_FILES += \
+	${bm_device}/bootstrap/images/background-def.png:system/bootstrap/images/background-def.png \
+	${bm_device}/bootstrap/images/background-blank.png:system/bootstrap/images/background-blank.png \
+
+# prebuilt binaries
+PRODUCT_COPY_FILES += \
+	${bm_device}/bootstrap/binary/logwrapper:system/bin/logwrapper \
+	${bm_device}/bootstrap/binary/logwrapper.bin:system/bin/logwrapper.bin \
+	${bm_device}/profiles/2nd-boot/hboot_recovery.cfg:system/bootstrap/2nd-boot/hboot_recovery.cfg \
+	${bm_device}/bootstrap/binary/hbootuser:system/bootstrap/2nd-boot/hbootuser \
+	${bm_device}/bootstrap/binary/safestrapmenu:system/bootstrap/2nd-boot/safestrapmenu \
+	${bm_device}/bootstrap/binary/busybox:system/bootstrap/binary/busybox \
+	$(bm_device)/twrp.fstab:recovery/root/etc/twrp.fstab \
+	$(OUT)/ramdisk-recovery.img:system/bootstrap/2nd-boot/ramdisk-recovery \
+	${bm_device}/profiles/2nd-boot/zImage-recovery:system/bootstrap/2nd-boot/zImage-recovery \
+	$(OUT)/ramdisk.img:system/bootstrap/2nd-boot/ramdisk \
+	$(OUT)/kernel:system/bootstrap/2nd-boot/zImage
+

device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)	
 # Blobs and bootmenu stuff
 $(call inherit-product, device/moto/shadow-common/shadow-blobs.mk)
-$(call inherit-product, device/moto/shadow-common/bootmenu/bootmenu.mk)
+$(call inherit-product, device/moto/shadow-common/bootstrap/bootstrap.mk)
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 # Get some sounds
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
@@ -104,7 +104,7 @@ PRODUCT_PACKAGES += \
 	libaudioutils audio.a2dp.default  \
 	libaudiohw_legacy \
 
-#	 OMX stuff
+# OMX stuff
 PRODUCT_PACKAGES += dspexec libbridge libLCML libOMX_Core libstagefrighthw
 PRODUCT_PACKAGES += libOMX.TI.AAC.encode libOMX.TI.AAC.decode libOMX.TI.AMR.decode libOMX.TI.AMR.encode
 PRODUCT_PACKAGES += libOMX.TI.WBAMR.encode libOMX.TI.MP3.decode libOMX.TI.WBAMR.decode
@@ -130,12 +130,7 @@ PRODUCT_PACKAGES += \
     hostapd.conf \
     wpa_supplicant.conf \
     regulatory.bin \
-    calibrator \
     ti_wfd_libs \
-
-PRODUCT_COPY_FILES += \
-    $(OUT)/ramdisk.img:system/bootmenu/2nd-boot/ramdisk \
-    $(OUT)/kernel:system/bootmenu/2nd-boot/zImage
 
 # Should be after the full_base include, which loads languages_full
 PRODUCT_LOCALES := en_US en_GB en_IN fr_FR it_IT de_DE es_ES hu_HU uk_UA zh_CN zh_TW ru_RU nl_NL se_SV cs_CZ pl_PL pt_BR da_DK ko_KR el_GR ro_RO iw_IL ar_EG