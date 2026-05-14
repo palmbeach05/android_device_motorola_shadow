Quarx2k/android_device_moto_jordan-common

Use 4.4.3 gcc for hboot

BoardConfig.mk
@@ -201,7 +201,7 @@ hboot:
 	mkdir -p $(PRODUCT_OUT)/system/bootstrap/2nd-boot   
 	echo "$(BOARD_KERNEL_CMDLINE)" > $(PRODUCT_OUT)/system//bootstrap/2nd-boot/cmdline  
 	echo "$(BOARD_RECOVERY_KERNEL_CMDLINE)" > $(PRODUCT_OUT)/system/bootstrap/2nd-boot/cmdline-recovery
-	make -C  $(ANDROID_BUILD_TOP)/device/moto/jordan-common/bootstrap/hboot ARCH=arm CROSS_COMPILE="arm-eabi-"
+	make -C  $(ANDROID_BUILD_TOP)/device/moto/jordan-common/bootstrap/hboot ARCH=arm CROSS_COMPILE=$(TARGET_KERNEL_MODULES_TOOLCHAIN)
 	mv $(ANDROID_BUILD_TOP)/device/moto/jordan-common/bootstrap/hboot/hboot.bin $(PRODUCT_OUT)/system/bootstrap/2nd-boot/
 	make clean -C $(ANDROID_BUILD_TOP)/device/moto/jordan-common/bootstrap/hboot
 

palmbeach05/android_device_motorola_shadow

BoardConfig.mk
@@ -201,7 +201,7 @@ hboot:
 	mkdir -p $(PRODUCT_OUT)/system/bootstrap/2nd-boot   
 	echo "$(BOARD_KERNEL_CMDLINE)" > $(PRODUCT_OUT)/system//bootstrap/2nd-boot/cmdline  
 	echo "$(BOARD_RECOVERY_KERNEL_CMDLINE)" > $(PRODUCT_OUT)/system/bootstrap/2nd-boot/cmdline-recovery
-	make -C  $(ANDROID_BUILD_TOP)/device/moto/shadow-common/bootstrap/hboot ARCH=arm CROSS_COMPILE="arm-eabi-"
+	make -C  $(ANDROID_BUILD_TOP)/device/moto/shadow-common/bootstrap/hboot ARCH=arm CROSS_COMPILE=$(TARGET_KERNEL_MODULES_TOOLCHAIN)
 	mv $(ANDROID_BUILD_TOP)/device/moto/shadow-common/bootstrap/hboot/hboot.bin $(PRODUCT_OUT)/system/bootstrap/2nd-boot/
 	make clean -C $(ANDROID_BUILD_TOP)/device/moto/shadow-common/bootstrap/hboot
 