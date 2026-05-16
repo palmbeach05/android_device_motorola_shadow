95dffb1627eabcb7a457a373afeae37d2a73eb37
palmbeach05 <5945226+palmbeach05@users.noreply.github.com>
2026-05-16 12:48:14 -0400

    Update WL127x binaries
    -TI WL127x multi-role firmware, version 6.5.7.0.50
     (ti-connectivity/wl127x-fw-5-mr.bin)
    -TI WL127x PLT (calibration) firmware, version 6.3.10.0.142
     (ti-connectivity/wl127x-fw-5-plt.bin)
    -TI WL127x single-role firmware, version 6.3.10.0.142
     (ti-connectivity/wl127x-fw-5-sr.bin)
    -TI WL127x reference NVS (ti-connectivity/wl127x-nvs.bin,
     (ti-connectivity/wl1271-nvs.bin, ti-connectivity/wl12xx-nvs.bin)
    
    Files renamed to -4- during build process because that is what it
    is requesting.

--- shadow.mk
+++ shadow.mk
@@ -41,10 +41,10 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # --- Hardware Blobs & Firmware ---
 # --- Connectivity (WLAN/BT Firmware) ---
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
-    $(DEVICE_PATH)/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
-    $(DEVICE_PATH)/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
-    $(DEVICE_PATH)/prebuilt/etc/firmware/ti-connectivity/wl1271-nvs.bin:system/etc/firmware/ti-connectivity/wl1271-nvs.bin \
+    $(DEVICE_PATH)/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-5-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
+    $(DEVICE_PATH)/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-5-plt.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
+    $(DEVICE_PATH)/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-5-sr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
+    $(DEVICE_PATH)/prebuilt/etc/firmware/ti-connectivity/wl127x-nvs.bin:system/etc/firmware/ti-connectivity/wl1271-nvs.bin \
     $(DEVICE_PATH)/prebuilt/etc/firmware/TIInit_7.6.15.bts:system/etc/firmware/TIInit_7.6.15.bts \
     $(DEVICE_PATH)/prebuilt/etc/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(DEVICE_PATH)/prebuilt/etc/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
